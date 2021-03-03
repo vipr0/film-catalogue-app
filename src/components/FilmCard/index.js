b8222bb57c471d80a9096e7eacb2f82f62ca24ac
Vitalii Protsyk <eqellez@gmail.com>
2021-03-03 21:34:17 +0200

    Removed button in film card

--- src/components/FilmCard/index.js
+++ src/components/FilmCard/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Card, Button, Statistic, Row, Col } from 'antd';
+import { Card, Statistic, Row, Col } from 'antd';
 import { Link } from "react-router-dom";
 
 function FilmCard({ film }) {
-    const {title, releaseYear, format, stars, _id: id} = film
+    const {title, releaseYear, format, _id: id} = film
 
     return (
-        <Card title={`"${title}"`}>
+        <Card title={<Link to={`/${id}`}>{title}</Link>}>
             <Row gutter={16} style={{ marginBottom: "10px" }}>
                 <Col span={12}>
                     <Statistic title="Release Year" value={releaseYear} groupSeparator="" />
@@ -15,10 +15,6 @@ function FilmCard({ film }) {
                     <Statistic title="Format" value={format} />
                 </Col>
             </Row>
-
-            <Link to={`/${id}`}>
-                <Button type="primary">Select</Button>
-            </Link>
         </Card>
     )
 }
