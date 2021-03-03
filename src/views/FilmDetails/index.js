9e9782c8cc513d3d2a7485d844f15d145c27bb90
Vitalii Protsyk <eqellez@gmail.com>
2021-03-03 21:03:23 +0200

    Added error handling for film details view

--- src/views/FilmDetails/index.js
+++ src/views/FilmDetails/index.js
@@ -1,4 +1,4 @@
-import { Button, PageHeader, Descriptions, message } from 'antd';
+import { Button, PageHeader, Descriptions, message, Empty } from 'antd';
 import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import Loader from '../../components/Loader';
@@ -13,11 +13,9 @@ function FilmDetails() {
     useEffect(() => {
         api
             .getFilm(filmId)
-            .then(({data}) => {
-                setIsLoading(false)
-                setFilm(data.data.film)
-            })
+            .then(({data}) => setFilm(data.data.film))
             .catch(err => message.error(err.message))
+            .finally(() => setIsLoading(false))
     }, [])
 
     const deleteFilm = () => {
@@ -33,29 +31,32 @@ function FilmDetails() {
     if(isLoading) {
         return (<Loader/>)
     } else {
-        return (
-            <div>
-                <PageHeader
-                    ghost={false}
-                    onBack={() => window.history.back()}
-                    title={film.title}
-                    subTitle={film._id}
-                    extra={[
-                        <Button onClick={deleteFilm} type="primary" danger>Delete film</Button>
-                    ]}
-                >
-                    <Descriptions size="small" column={1}>
-                        <Descriptions.Item label="Release Year">{film.releaseYear}</Descriptions.Item>
-                        <Descriptions.Item label="Format">{film.format}</Descriptions.Item>
-                        <Descriptions.Item label="Stars">
-                            {film.stars && film.stars.map(star => (<div className="star-list__item">{star}</div>))}
-                        </Descriptions.Item>
-                    </Descriptions>
-                </PageHeader>
-            </div>
-        )
+        if(Object.keys(film).length > 0) {
+            return (
+                <div>
+                    <PageHeader
+                        ghost={false}
+                        onBack={() => window.history.back()}
+                        title={film.title}
+                        subTitle={film._id}
+                        extra={[
+                            <Button onClick={deleteFilm} type="primary" danger>Delete film</Button>
+                        ]}
+                    >
+                        <Descriptions size="small" column={1}>
+                            <Descriptions.Item label="Release Year">{film.releaseYear}</Descriptions.Item>
+                            <Descriptions.Item label="Format">{film.format}</Descriptions.Item>
+                            <Descriptions.Item label="Stars">
+                                {film.stars && film.stars.map(star => (<div className="star-list__item">{star}</div>))}
+                            </Descriptions.Item>
+                        </Descriptions>
+                    </PageHeader>
+                </div>
+            )
+        } else {
+            return (<Empty/>)
+        }
     }
-
 }
 
 export default FilmDetails
